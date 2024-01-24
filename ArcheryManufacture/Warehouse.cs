11ce313010215e223873f9c9e14c59503b19d48b
PawelRutkovvski <parutkowski@yahoo.com>
2024-01-24 23:37:40 +0100

    ArcheryManufacture (add classes)

---
+++ ArcheryManufacture/Warehouse.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcheryManufacture
+{
+    public class Warehouse
+    {
+        private List<string> items = new List<string>();
+
+        public void DisplayItems()
+        {
+            Console.WriteLine("So... What We Have Here:");
+            if (items.Count == 0)
+            {
+                Console.WriteLine("Warehouse Is Empty\n");
+            }
+            else
+            {
+                foreach (string item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public void AddItem(string itemName)
+        {
+            items.Add(itemName);
+        }
+
+        public void RemoveItem(string itemName)
+        {
+            if (items.Contains(itemName))
+            {
+                items.Remove(itemName);
+                Console.WriteLine($"Item {itemName} Has Been Removed");
+            }
+            else
+            {
+                Console.WriteLine($"{itemName} Is Not In Warehouse");
+            }
+        }
+
+        public bool ContainsItems(List<string> requiredItems)
+        {
+            foreach (var item in requiredItems)
+            {
+                if (!items.Contains(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void RemoveItems(List<string> itemsToRemove)
+        {
+            foreach (var item in itemsToRemove)
+            {
+                items.Remove(item);
+            }
+        }
+    }
+}
---
+++ ArcheryManufacture/Workshop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcheryManufacture
+{
+    public class Workshop
+    {
+        public void CreateBow(Warehouse warehouse)
+        {
+            Console.WriteLine("If You Want To Create The Bow - You Need Below Mentioned Materials: \r\n WOOD, THREAD, OIL, ANIMAL HORN.");
+            Console.WriteLine("= = = = = = = = ");
+            Console.WriteLine("Do You Want To Try To Create A Bow ? ");
+            Console.Write("Type YES or NO : ");
+            var yesOrNoType = Console.ReadLine().ToUpper();
+
+            if (yesOrNoType == "YES")
+            {
+                bool itemsForBow = warehouse.ContainsItems(new List<string> { "WOOD", "THREAD", "OIL", "ANIMAL HORN" });
+
+                if (itemsForBow)
+                {
+                    Console.WriteLine("You Have All Needed Materials, so You Created The Bow. \r\n CONGRATULATION :-) ");
+                    Console.WriteLine("The BOW Has Been Added to Warehouse");
+                    warehouse.AddItem("BOW");
+                    warehouse.RemoveItems(new List<string> { "WOOD", "THREAD", "OIL", "ANIMAL HORN" });
+                }
+                else
+                {
+                    Console.WriteLine("So Sorry, But You Do Not Have All Needed Materials.");
+                    Console.WriteLine("You Can Not Create A Bow :-/ ");
+                }
+            }
+        }
+
+        // Metody dla innych przedmiotów warsztatu można dodać podobnie
+        // ...
+
+        // Inne metody związane z funkcjonalnością warsztatu można dodać tutaj
+    }
+}
